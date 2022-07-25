9497e9d0357c62cb30c80674270034560ad55d56
Daniel Santana <daniel.santana.silva@uninove.edu.br>
2022-07-25 16:40:03 -0300

    alteração em type-null-endefined

--- type-null-undefined.js
+++ type-null-undefined.js
@@ -12,7 +12,7 @@ if (input === null) {
 
 // É aqui que entra o tipo undefined. Este tipo também representa “ausência de valor”, porém de uma outra forma: usualmente, enquanto null é um valor atribuído a uma variável que existe e foi iniciada, undefined se refere ao valor de uma variável que não foi inicializada (ou seja, não foi atribuído nenhum valor a ela).
 
-let input = null;
+let input1 = null;
 let input2;
 
 console.log(input); // null
